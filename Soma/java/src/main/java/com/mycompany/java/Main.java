ecfb0c922e2cd79e87409320cb3cab50572e94b7
Fabiano Mendes <fabianomendesoficial@gmail.com>
2021-07-31 21:01:58 -0300

    solving sum exercise in java

---
+++ Soma/java/src/main/java/com/mycompany/java/Main.java
@@ -0,0 +1,12 @@
+package com.mycompany.java;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+        
+        System.out.println(x+y);
+    }
+}
