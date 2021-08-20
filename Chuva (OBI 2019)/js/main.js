b83d29a05e34ab76aefc197326c0f56e894e2a4a
Fabiano Mendes <fabianomendesoficial@gmail.com>
2021-08-19 22:23:34 -0300

    start Chuva (OBI 2019)

---
+++ Chuva (OBI 2019)/js/main.js
@@ -0,0 +1,12 @@
+let fs = require("fs")
+let matriz = fs.readFileSync(0,"utf8")
+
+for(let i=0; i < matriz.length; i++){
+    for(let j=0; j < matriz[i].length; j++){
+        
+    }
+}
+
+
+
+console.log(matriz)
