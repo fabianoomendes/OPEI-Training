0ebf7e11b98f86efd8f3c334e591e4afa708ed76
Fabiano Mendes <fabianomendesoficial@gmail.com>
2021-08-22 03:27:37 -0300

    Modification in Quadrado Mágico

--- Quadrado Mágico (OBI 2007)/js/main.js
+++ Quadrado Mágico (OBI 2007)/js/main.js
@@ -15,58 +15,54 @@ for(let i=0; i < matriz[0].length; i++){
     sum += matriz[0][i]
 }
 
-let result = sum
+let result = true
 
 for(let i=1; i < matriz.length; i++){
     let aux = 0
     for(let j=0; j < matriz[i].length; j++){
         aux += matriz[i][j]
     }
-    if(sum === aux){
-        result = sum
-    }else{
-        result = -1
+    if(sum !== aux){
+        result = false
         break
     }
 }
 
-if(result === -1){
+if(result){
     for(let i=0; i < matriz[0].length; i++){
         let aux = 0
         for(let j=0; j < matriz.length; j++){
             aux += matriz[j][i]
         }
-        if(sum === aux){
-            result = sum
-        }else{
-            result = -1
+        if(sum !== aux){
+            result = false
             break
         }
     }
 }
 
-if(result === -1){
+if(result){
     let aux = 0
     for(let i=0; i < matriz.length; i++){        
         aux += matriz[i][i]        
     }    
-    if(sum === aux){
-        result = sum
-    }else{
-        result = -1
+    if(sum !== aux){
+        result = false
     }
 }
 
-if(result === -1){
+if(result){
     let aux = 0
     for(let i=matriz.length-1; i >= 0; i--){        
         aux += matriz[i][i]        
     }    
-    if(sum === aux){
-        result = sum
-    }else{
-        result = -1
+    if(sum !== aux){
+        result = false
     }
 }
 
-console.log(result)
+if(result){
+    console.log(sum)
+}else{
+    console.log(-1)
+}
